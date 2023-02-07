cce6aa5906a394e893db362630493457298d16bf
felicia <feli5659@stud.kea.dk>
2023-02-07 13:50:22 +0100

    iterator works

--- typewriter.js
+++ typewriter.js
@@ -1,2 +1,28 @@
 "use strict";
 
+let iterator = 0;
+let typewriterText;
+
+const typewriter = document.querySelector("#typewriter");
+
+initLoop();
+
+function initLoop() {
+  console.log("initLoop is called");
+  loop();
+}
+
+function loop() {
+  typewriterText = typewriter.textContent;
+  let maxNumberOfItems = typewriterText.length;
+
+  iterator++;
+
+  if (iterator <= maxNumberOfItems) {
+    setTimeout(loop, 200);
+  }
+//   Array.from(typewriterText).forEach((letter, i) => {
+    
+//   });
+  console.log(typewriterText);
+}
