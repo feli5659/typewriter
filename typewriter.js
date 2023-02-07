e49161fcfe27f5f21ee9d4bf2462506e796ba947
felicia <feli5659@stud.kea.dk>
2023-02-07 15:34:05 +0100

    finished

--- typewriter.js
+++ typewriter.js
@@ -1,27 +1,27 @@
 "use strict";
 
 let iterator = 0;
-let typewriterText;
+
 let letter;
 const typewriter = document.querySelector("#typewriter");
-typewriterText = typewriter.textContent;
+let typewriterText = typewriter.textContent;
 const maxNumberOfItems = typewriterText.length;
-console.log(typewriterText);
+typewriter.textContent = "";
 initLoop();
 
 function initLoop() {
-  console.log("initLoop is called");
   loop();
 }
 
 function loop() {
   iterator++;
   letter = "";
+  
 
   if (iterator <= maxNumberOfItems) {
     setTimeout(loop, 200);
     letter = typewriterText[iterator - 1];
     typewriter.textContent += letter;
-  } 
+  }
   console.log(letter);
 }
